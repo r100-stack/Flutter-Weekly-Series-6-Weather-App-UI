119e7cec802741666e0715fbb28387142f37d0fe
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-05 16:01:01 -0600

    Added solution for Step 3

--- lib/screens/home_screen/components/weather_card.dart
+++ lib/screens/home_screen/components/weather_card.dart
@@ -25,46 +25,46 @@ class WeatherCard extends StatelessWidget {
       // TODO (9): Wrap the Container in ClipRRect => circular, kDefaultBorderRadius
       // TODO (10): Inner Container decoration(0xFF427BFF, CircularBorder(kDefaultBorderRadius))
 
-      // child: ClipRRect(
-      //   borderRadius: BorderRadius.circular(kDefaultBorderRadius),
-      //   child: Container(
-      //     decoration: BoxDecoration(
-      //       color: Color(0xFF427BFF),
-      //       borderRadius: BorderRadius.circular(kDefaultBorderRadius),
-      //     ),
-      //     child: Column(
-      //       children: [
-      //         const SizedBox(height: 20.0),
-      //         Image(
-      //           height: 100.0,
-      //           width: 150.0,
-      //           fit: BoxFit.cover,
-      //           image: AssetImage(
-      //             'assets/images/cloudy_with_sun.png',
-      //           ),
-      //         ),
-      //         Text(
-      //           'Heavy Rain',
-      //           style: GoogleFonts.raleway(
-      //             color: Color(0xFFE2EBFF),
-      //             fontSize: 24.0,
-      //             fontWeight: FontWeight.bold,
-      //           ),
-      //         ),
-      //         const SizedBox(height: 10.0),
-      //         Text(
-      //           'Sunday, 02 Oct',
-      //           style: GoogleFonts.raleway(
-      //             color: Color(0xFF7EA5FF),
-      //             fontSize: 16.0,
-      //             fontWeight: FontWeight.w400,
-      //           ),
-      //         ),
-      //         const SizedBox(height: 20.0),
-      //       ],
-      //     ),
-      //   ),
-      // ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(kDefaultBorderRadius),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF427BFF),
+            borderRadius: BorderRadius.circular(kDefaultBorderRadius),
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 20.0),
+              Image(
+                height: 100.0,
+                width: 150.0,
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/images/cloudy_with_sun.png',
+                ),
+              ),
+              Text(
+                'Heavy Rain',
+                style: GoogleFonts.raleway(
+                  color: Color(0xFFE2EBFF),
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Text(
+                'Sunday, 02 Oct',
+                style: GoogleFonts.raleway(
+                  color: Color(0xFF7EA5FF),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+            ],
+          ),
+        ),
+      ),
 
     );
   }
