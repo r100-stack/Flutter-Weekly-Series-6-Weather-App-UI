9be7ca13571c9904aee0cf17bfdbac5d194839a4
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2021-03-08 22:39:26 -0600

    Added TODOs for step 8

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -6,72 +6,59 @@ import 'package:weather_app_6/screens/home_screen/components/hourly_weather_bar.
 import 'package:weather_app_6/screens/home_screen/components/weather_card.dart';
 
 class HomeScreen extends StatelessWidget {
-  Widget _buildMainCard() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(height: 20.0),
-          Row(
-            children: [
-              Text(
-                'Bandung,',
-                style: GoogleFonts.raleway(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25.0,
-                  color: Color(0xFF333333),
-                ),
-              ),
-              const SizedBox(width: 8.0),
-              Text(
-                'Indonesia',
-                style: GoogleFonts.raleway(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 21.0,
-                  color: Color(0xFF676767),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 30.0),
-          WeatherCard(),
-          const SizedBox(height: 30.0),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildColumn() {
-    return Column(
-      children: [
-        _buildMainCard(),
-        HourlyWeatherBar(padding: const EdgeInsets.symmetric(horizontal: 12.0)),
-        const SizedBox(height: 30.0),
-      ],
-    );
-  }
-
-  Widget _buildRow() {
-    return Row(
-      children: [
-        Expanded(child: _buildMainCard()),
-        Expanded(
-            child: HourlyWeatherBar(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0))),
-        const SizedBox(height: 30.0),
-      ],
-    );
-  }
+  // TODO (3): Create _buildRow()
+  // TODO (4): Return the same as _buildColumn() but with Row
+  // TODO (5): Wrap each widget with expanded
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-
     return Scaffold(
       appBar: CustomAppBar(),
       body: SingleChildScrollView(
-        child: width <= 800 ? _buildColumn() : _buildRow(),
+        // TODO (6): If width <= 800, child: _buildColumn(), else, child: _buildRow()
+
+        // TODO (2): Extract this widget to _buildColumn()
+        child: Column(
+          children: [
+            // TODO (1): Extract this widget to _buildMainCard()
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 20.0),
+                  Row(
+                    children: [
+                      Text(
+                        'Bandung,',
+                        style: GoogleFonts.raleway(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0,
+                          color: Color(0xFF333333),
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      Text(
+                        'Indonesia',
+                        style: GoogleFonts.raleway(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 21.0,
+                          color: Color(0xFF676767),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30.0),
+                  WeatherCard(),
+                  const SizedBox(height: 30.0),
+                ],
+              ),
+            ),
+            HourlyWeatherBar(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0)),
+            const SizedBox(height: 30.0),
+          ],
+        ),
       ),
     );
   }
